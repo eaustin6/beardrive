2114b360e42f8620c148cd3d3a96b6f193ebbb39
Sito Mao aka Eric Austin <86404384+eaustin6@users.noreply.github.com>
2024-01-21 22:52:50 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,17 +1,17 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
-    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    API_ID = int(env.get("TELEGRAM_API_ID", 1747534))
+    API_HASH = env.get("TELEGRAM_API_HASH", "5a2684512006853f2e48aca9652d83ea")
+    OWNER_ID = int(env.get("OWNER_ID", 5748909356))
+    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "5748909356").split()
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "kumastreamxbot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6554978132:AAGzmfLaNZKOl3cuRuAgshXdOJInOqNmzKw")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001924574319))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("drive.bearzap.me", "http://127.0.0.1:8080")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
